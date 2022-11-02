4e8117c0ed224bb74cae38c9c928bd53af3b8132
David Last <lastd44@gmail.com>
2022-11-02 03:26:30 -0500

    Scale 1D histograms by fit results.

--- ScaleHistos.cxx
+++ ScaleHistos.cxx
@@ -1,7 +1,7 @@
-//File: CombineTgts.cxx
-//Info: This is a script to add up all the histograms across various files so that the materials in the targets are combined.
+//File: ScaleHistos.cxx
+//Info: This script takes an input MC file and a scale factors file to produce a copy with the right histos scaled
 //
-//Usage: CombineTgts <data/MC> <Material> <outDir> <Tgt1 FileName>
+//Usage: ScaleHistos <inFile> <scaleFile> <fitName> <outFile>
 //Author: David Last dlast@sas.upenn.edu/lastd44@gmail.com
 
 //C++ includes
@@ -105,230 +105,91 @@ int main(int argc, char* argv[]) {
     return 2;
   }
 
-  bool isMC = ((TString)(argv[1]) == "MC") ? true : false;
-  TString material = argv[2];
-  TString matName = (material != "Water")? "_"+material : "";
-  string outDir=string(argv[3]);
-  string baseFileName=argv[4];
+  TString inFileName = argv[1];
+  TString scaleFileName = argv[2];
+  TString fitName = argv[3];
+  TString outFileName = argv[4];
 
-  if (material != "Fe" && material != "C" && material != "Pb" && material != "Water"){
-    cout << "Can't handle that material yet." << endl;
-    return -999;
-  }
+  TFile* inFile = new TFile(inFileName,"READ");
+  TFile* scaleFile = new TFile(scaleFileName,"READ");
+  TFile* outFile = new TFile(outFileName,"RECREATE");
 
-  if (PathExists(outDir)){
-    cout << "Thank you for choosing a path for output files that exists." << endl;
-  }
-  else{
-    cout << "Output directory doesn't exist. Exiting" << endl;
+  TList* scaleList = scaleFile->GetListOfKeys();
+  if (!scaleList){
+    cout << "issue with scale File" << endl;
     return 3;
   }
 
-  string rootExt = ".root";
-  string slash = "/";
-  string token;
-  string fileNameStub = baseFileName;
-  size_t pos=0;
-
-  if (isMC){
-    if ((pos = baseFileName.find("MC")) == string::npos){
-      cout << "File is Not MC, and should be." << endl;
-      return 4;
-    }
-  }
-  else if ((pos = baseFileName.find("Data")) == string::npos){
-    cout << "File is Not Data, and should be." << endl;
-    return 4;
-  }
-
-  pos = 0;
-  //cout << sigNameStub << endl;
-  while ((pos = fileNameStub.find(slash)) != string::npos){
-    //cout << sigNameStub << endl;
-    token = fileNameStub.substr(0, pos);
-    //cout << token << endl;
-    fileNameStub.erase(0, pos+slash.length());
-  }
-  //cout << sigNameStub << endl;
-  if ((pos=fileNameStub.find(rootExt)) == string::npos){
-    cout << "Input need be .root file." << endl;
-    return 5;
-  }
-
-  cout << "Input file name parsed to: " << fileNameStub << endl;
-
-  TString tgtFind = "Tgt1";
-
-  vector<TString> outFileNamePieces = BreakName(tgtFind,fileNameStub);
-  vector<TString> fileNamePieces = BreakName(tgtFind,baseFileName);
-  
-  cout << "Input Name reclaimed as: " << MashNames("Tgt1",fileNamePieces) << endl;
-
-  TString outName = MashNames(material,outFileNamePieces);
-  TFile* outFile = new TFile(outDir+outName,"RECREATE");
-
-  std::map<TString,TFile*> files;
-  for (auto tgt : tgtsByMat[material]){
-    TString tgtName = MashNames(tgt,fileNamePieces);
-    files[tgt] = new TFile(tgtName,"READ");
-  }
-
-  TString firstName = tgtsByMat[material].at(0);
-  TString dirBase = "ByTgt_";
-
-  TDirectoryFile* dir = (TDirectoryFile*)files[firstName]->Get(dirBase+firstName+matName);
-  if (!dir){
-    cout << "Could not find material in the first Tgt. Exiting Now" << endl;
-    return 18;
-  }
- 
-  TList* keyList = dir->GetListOfKeys();
-  if(!keyList){
-    cout << "List of keys failed to get inside directory." << endl;
-    return 19;
-  }
-
-  TIter nextKey(keyList);
-  TKey* key;
-  while ( key = (TKey*)nextKey() ){
-    TString className = (TString)key->GetClassName();
-    TString nameObj = (TString)key->GetName();
-    cout << "AT Object: " << nameObj << endl;
-
-    if (className == "TDirectoryFile"){
-      TDirectory* newOutDir = outFile->mkdir(nameObj);
-      TDirectoryFile* dirInt = (TDirectoryFile*)files[firstName]->Get(dirBase+firstName+matName+"/"+nameObj);
-      TList*  keyIntList = dirInt->GetListOfKeys();
-      if(!keyIntList){
-	cout << "List of keys failed to get inside second directory" << endl;
-	return 20;
-      }
-      TIter nextKeyInt(keyIntList);
-      TKey* keyInt;
-      while ( keyInt = (TKey*)nextKeyInt() ){
-	TString classNameInt = (TString)keyInt->GetClassName();
-	TString nameObjInt = (TString)keyInt->GetName();
-	cout << "AT Object: " << nameObjInt << endl;
-	if (!classNameInt.Contains("PlotUtils::MnvH1")) continue;
-	cout << "Getting: " << nameObjInt << endl;
-	cout << "" << endl;
-	vector<TString> nameObjIntPieces = BreakName(firstName+matName,nameObjInt);
-	TString matNameObjInt = MashNames(material,nameObjIntPieces);
-	
-	if (nameObj.Contains("US_ByType")){
-	  MnvH1D* h1D = nullptr;
-	  bool first = true;
-	  for (auto tgt : UStgtsByMat[material]){
-	    cout << "US of Tgt: " << tgt << endl;
-	    if (first){
-	      TString newNameObjInt = MashNames(tgt+matName,nameObjIntPieces);
-	      h1D = (MnvH1D*)(files[tgt]->Get(dirBase+tgt+matName+"/"+nameObj+"/"+newNameObjInt))->Clone(matNameObjInt);
-	      first = false;
-	    }
-	    else{
-	      TString newNameObjInt = MashNames(tgt+matName,nameObjIntPieces);
-	      h1D->Add((MnvH1D*)(files[tgt]->Get(dirBase+tgt+matName+"/"+nameObj+"/"+newNameObjInt)));
-	    }
-	  }
-	  newOutDir->cd();
-	  h1D->Write();
-	  delete h1D;
-	}
-	
-	if (nameObj.Contains("DS_ByType")){
-	  MnvH1D* h1D = nullptr;
-	  bool first = true;
-	  for (auto tgt:DStgtsByMat[material]){
-	    cout << "DS of Tgt: " << tgt << endl;
-	    if (first){
-	      TString newNameObjInt = MashNames(tgt+matName,nameObjIntPieces);
-	      h1D = (MnvH1D*)(files[tgt]->Get(dirBase+tgt+matName+"/"+nameObj+"/"+newNameObjInt))->Clone(matNameObjInt);
-	      first = false;
-	    }
-	    else{
-	      TString newNameObjInt = MashNames(tgt+matName,nameObjIntPieces);
-	      h1D->Add((MnvH1D*)(files[tgt]->Get(dirBase+tgt+matName+"/"+nameObj+"/"+newNameObjInt)));
-	    }
-	  }
-	  newOutDir->cd();
-	  h1D->Write();
-	  delete h1D;
-	}
-      }
-      //continue;
-    }
-
-    else if (!className.Contains("PlotUtils::MnvH")) continue;
-    cout << "Getting: " << nameObj << endl;
+  TIter nextScale(scaleList);
+  TKey* scale;
+  map<TString,vector<TString>> varTagsMap;
+  map<TString,TString> varNameMap;
+  while ( scale = (TKey*)nextScale() ){
+    TString scaleName = (TString)scale->GetName();
+    if (!scaleName.Contains("fit_"+fitName)) continue;
+    cout << scaleName << endl;
+    vector<TString> fitPieces = BreakName("fit_"+fitName+"_t_",scaleName);
+    vector<TString> varNamePieces = BreakName("_",fitPieces.at(0));
+    TString varName = varNamePieces.at(0);
+    cout << varName << endl;
+    TString tagChunk = fitPieces.at(1);
+    vector<TString> tags = BreakName("_t_",tagChunk);
+    //TODO: Make able to handle more complicated case of not just using the fit from 500-1000 of just the background categories.
+    for(auto tag: tags) varTagsMap[varName].push_back(tag);
+    varNameMap[varName+tags.at(0)] = scaleName;
     cout << "" << endl;
-    vector<TString> nameObjPieces = BreakName(firstName+matName,nameObj);
-    TString matNameObj = MashNames(material,nameObjPieces);
-    if (className.Contains("2")){
-      MnvH2D* h2D = (MnvH2D*)(files[firstName]->Get(dirBase+firstName+matName+"/"+nameObj))->Clone(matNameObj);
-      //cout << "First Histo has Integral: " << h2D->Integral() << endl;
-      //cout << "" << endl;
-      for(auto file : files){
-	if (file.first == firstName) continue;
-	//cout << "Getting: " << file.first << endl;
-	TString newNameObj = MashNames(file.first+matName,nameObjPieces);
-	h2D->Add((MnvH2D*)(file.second->Get(dirBase+file.first+matName+"/"+newNameObj)));
-      }
-      //h2D->SetDirectory(outFile);
-      outFile->cd();
-      h2D->Write();
-      delete h2D;
-    }
-    else{
-      MnvH1D* h1D = (MnvH1D*)(files[firstName]->Get(dirBase+firstName+matName+"/"+nameObj))->Clone(matNameObj);
-      for(auto file : files){
-	if (file.first == firstName) continue;
-	TString newNameObj = MashNames(file.first+matName,nameObjPieces);
-	h1D->Add((MnvH1D*)(file.second->Get(dirBase+file.first+matName+"/"+newNameObj)));
-      }
-      //h1D->SetDirectory(outFile);
-      outFile->cd();
-      h1D->Write();
-      delete h1D;
-    }
   }
 
-  cout << "Writing File" << endl;
-  //outFile->Write();
-
-  cout << "File Written" << endl;
-  /*
-  TList* keyList = mcFile->GetListOfKeys();
+  TList* keyList = inFile->GetListOfKeys();
   if (!keyList){
-    cout << "List of keys failed to get." << endl;
-    return 5;
+    cout << "issue with input file" << endl;
+    return 4;
   }
 
   TIter next(keyList);
   TKey* key;
+  while ( key = (TKey*)next() ){
+    TString className = (TString)key->GetClassName();
+    TString nameObj = (TString)key->GetName();
+    if (!className.Contains("MnvH") || nameObj.Contains("MYBins")) continue;
+    else if (className.Contains("MnvH2")){
+      MnvH2D* h2D = (MnvH2D*)(inFile->Get(nameObj))->Clone(nameObj);
+      outFile->cd();
+      h2D->Write();
+      delete h2D;
+    }
+    else if (className.Contains("MnvH1")){
+      bool scaled = false;
+      MnvH1D* h1D = (MnvH1D*)(inFile->Get(nameObj))->Clone(nameObj);
+      for (auto varName: varTagsMap){
+	if (!nameObj.Contains(varName.first) || scaled) continue;
+	for (auto tag: varName.second){
+	  if(!nameObj.Contains(tag)) continue;
+	  cout << "Scaling: " << nameObj << endl;
+	  TString nameOfScale = varNameMap[varName.first+varName.second.at(0)];
+	  MnvH1D* hScale = (MnvH1D*)scaleFile->Get(nameOfScale);
+	  h1D->Multiply(h1D,hScale);
+	  delete hScale;
+	  scaled = true;
+	  break;
+	}
+      }
+      outFile->cd();
+      h1D->Write();
+      delete h1D;
+    }
+    else{
+      cout << "HUH?" << endl;
+    }
   }
-  */
-  //cout << "Deleting the MnvPlotter." << endl;
-  //delete plotter;
 
-  //delete mcPOT;
-  //delete
-  cout << "Changing Dir. " << endl;
-  outFile->cd();
-  cout << "Getting POT" << endl;
-  TParameter<double>* POT = (TParameter<double>*)(files[firstName]->Get("POTUsed"))->Clone("POTUsed");
-  cout << "Writing POT" << endl;
-  POT->Write();
   cout << "Closing outFile" << endl;
   outFile->Close();
 
-  cout << "Closing Files... Does this solve the issue of seg fault." << endl;
-  for (auto file : files){
-    file.second->Close();
-  } 
+  cout << "Closing input files" << endl;
+  inFile->Close();
+  scaleFile->Close();
 
-  //mcFile->Close();
-  //dataFile->Close();
-
-  cout << "HEY YOU DID IT!!!" << endl;
+  cout << "HEY YOU DID IT" << endl;
   return 0;
 }
