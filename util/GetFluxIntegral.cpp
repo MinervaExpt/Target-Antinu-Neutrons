5cbe6243775c4d2b5faad28a362ef9e5b8787ba4
David Last <lastd44@gmail.com>
2023-01-30 12:51:56 -0600

    Updated to get flux for antineutrino events.

--- util/GetFluxIntegral.cpp
+++ util/GetFluxIntegral.cpp
@@ -23,7 +23,7 @@ namespace util
   PlotUtils::MnvH1D* GetFluxIntegral(const CVUniverse& univ, PlotUtils::MnvH1D* templateHist, const double Emin /*GeV*/, const double Emax /*GeV*/)
   {
     const bool useMuonCorrelations = true;
-    assert(!(useMuonCorrelations && (univ.GetAnalysisNuPDG() < 0)) && "Muon momentum correlations are not yet ready for ME antineutrino analyses!");
+    //assert(!(useMuonCorrelations && (univ.GetAnalysisNuPDG() < 0)) && "Muon momentum correlations are not yet ready for ME antineutrino analyses!");
 
     auto& frw = PlotUtils::flux_reweighter(univ.GetPlaylist(), univ.GetAnalysisNuPDG(), univ.UseNuEConstraint(), univ.GetNFluxUniverses());
     return frw.GetIntegratedFluxReweighted(univ.GetAnalysisNuPDG(), templateHist, Emin, Emax, useMuonCorrelations);
