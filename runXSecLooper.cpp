bf4c4c7a4ab90c48910dac4cfb16cbc53f7b717f
David Last <lastd44@gmail.com>
2023-08-17 17:40:37 -0500

    Changes for running the XSec Looper for 1D CCQELike, 1+ neutrons in pTmu Analysis in the tracker region.

--- runXSecLooper.cpp
+++ runXSecLooper.cpp
@@ -17,31 +17,71 @@ public:
   {
   };
 
-bool isCCInclusiveSignal( ChainWrapper& chw, int entry )
-{
-  double theta              = 0.;
-  double true_muon_px   = (double)chw.GetValue("mc_primFSLepton",entry,0)/1000;
-  double true_muon_py   = (double)chw.GetValue("mc_primFSLepton",entry,1)/1000;
-  double true_muon_pz   = (double)chw.GetValue("mc_primFSLepton",entry,2)/1000;
-  double numi_beam_angle_rad = -0.05887;
-  double pyprime = -1.0*sin(numi_beam_angle_rad)*true_muon_pz + cos(numi_beam_angle_rad)*true_muon_py;
-  double pzprime =  1.0*cos(numi_beam_angle_rad)*true_muon_pz + sin(numi_beam_angle_rad)*true_muon_py;
-  double pSquare = pow(true_muon_px,2) + pow(pyprime,2) + pow(pzprime,2);
-  theta = acos( pzprime / sqrt(pSquare) );
-  theta *= 180./3.14159;
+  bool isCCInclusiveSignal( ChainWrapper& chw, int entry )
+  {
+    double theta              = 0.;
+    double true_muon_px   = (double)chw.GetValue("mc_primFSLepton",entry,0)/1000;
+    double true_muon_py   = (double)chw.GetValue("mc_primFSLepton",entry,1)/1000;
+    double true_muon_pz   = (double)chw.GetValue("mc_primFSLepton",entry,2)/1000;
+    double numi_beam_angle_rad = -0.05887;
+    double pyprime = -1.0*sin(numi_beam_angle_rad)*true_muon_pz + cos(numi_beam_angle_rad)*true_muon_py;
+    double pzprime =  1.0*cos(numi_beam_angle_rad)*true_muon_pz + sin(numi_beam_angle_rad)*true_muon_py;
+    double pSquare = pow(true_muon_px,2) + pow(pyprime,2) + pow(pzprime,2);
+    double pMag = sqrt(pSquare);
+    theta = acos( pzprime / pMag );
+    theta *= 180./3.14159;
+    
+    //if(!chw.GetValue("truth_is_fiducial",entry)) return false; //Doesn't work for MasterAnaDev tuples.  What does this even mean in the targets anyway? :(
+    //if( pzprime >= 1.5 && theta <= 20.0 ) return true;
+    if( pMag > 1.5 && pMag < 20.0 && theta <= 17.0 ) return true;
+    return false;
+    
+  }
+  
+  bool isFSSignal( ChainWrapper& chw, int entry )
+  {
+    int genie_n_muons = 0;
+    int genie_n_mesons = 0;
+    int genie_n_heavy_baryons_plus_pi0s = 0;
+    int genie_n_photons = 0;
+    int genie_n_protons = 0;
+    int genie_n_neutrons = 0;
 
-  //if(!chw.GetValue("truth_is_fiducial",entry)) return false; //Doesn't work for MasterAnaDev tuples.  What does this even mean in the targets anyway? :(
-  if( pzprime >= 1.5 && theta <= 20.0 ) return true;
-  return false;
+    int nFSPart = (int)chw.GetValue("mc_nFSPart", entry);
 
-}
+    for (unsigned int i=0; i<nFSPart; ++i){
+      int pdg = (int)chw.GetValue("mc_FSPartPDG",entry,i);
+      double energy = (double)chw.GetValue("mc_FSPartE",entry,i);
+      double proton_E = 1058.272;
+      double neutron_E = 939.57+10.0;//Hard-coded neutKE for now.
+      if (abs(pdg) == 13) genie_n_muons++;
+      else if ( pdg == 22  && energy > 10) genie_n_photons++;
+      else if ( abs(pdg) == 211 || abs(pdg) == 321 || abs(pdg) == 323 || pdg == 111 || pdg == 130 || pdg == 310 || pdg == 311 || pdg == 313 ){
+	genie_n_mesons++;
+      }
+      else if ( pdg == 3112 || pdg == 3122 || pdg == 3212 || pdg == 3222 || pdg == 4112 || pdg == 4122 || pdg == 4222 || pdg == 411 || pdg == 421 || pdg == 111){
+	genie_n_heavy_baryons_plus_pi0s++;
+      }
+      else if ( pdg == 2212 && energy > proton_E) genie_n_protons++;
+      else if ( pdg == 2112 && energy > neutron_E) genie_n_neutrons++;
+    }
+
+      return genie_n_muons == 1 &&
+	genie_n_mesons == 0 &&
+	genie_n_heavy_baryons_plus_pi0s == 0 &&
+	genie_n_photons == 0 &&
+	genie_n_protons == 0 &&
+	genie_n_neutrons > 0; //Hard-coded to include > 0 neutrons for now.
+  }
+
   // Override this method from the base class to decide what events to
   // include in this selection
   virtual bool passesCuts(ChainWrapper& chw, int entry)
   {
-    if((int)chw.GetValue("mc_incoming", entry)!=14) return false;
+    if((int)chw.GetValue("mc_incoming", entry)!=-14) return false;
     if((int)chw.GetValue("mc_current", entry)!=1) return false;
     if(!isCCInclusiveSignal  ( chw, entry ) ) return false;
+    if(!isFSSignal(chw, entry)) return false;
     
     return true;
   }
@@ -50,31 +90,47 @@ bool isCCInclusiveSignal( ChainWrapper& chw, int entry )
 int main(const int argc, const char** argv)
 {
   //Read a playlist file from the command line
-  if(argc != 2)
+  if(argc < 3)
   {
-    std::cerr << "Expected exactly 1 command line argument, but got " << argc - 1 << ".\n\n"
-              << "USAGE: runXSecLooper <MCPlaylist.txt>\n\n"
-              << "MCPlaylist.txt shall contain one .root file per line that has a Truth tree in it.\n"
+    std::cerr << "Expected at least 2 command line argument, but got " << argc - 1 << ".\n\n"
+              << "USAGE: runXSecLooper <outName> <MCPlaylists.txt>\n\n"
+              << "MCPlaylists.txt are a list of files which shall contain one .root file per line that has a Truth tree in it.\n"
               << "This program returns 0 when it suceeds.  It produces a .root file with GENIEXSECEXTRACT in its name.\n";
     return 1;
   }
 
-  const std::string playlistFile = argv[1]; //argv[0] is the name of the executable
+  string outName = argv[1];
+
+  std::vector<const char*> fileNames(argv+2,argv+argc);
 
   // Create the XSecLooper and tell it the input files
   // Inputs should be the merged ntuples:
-  XSecLooper loop(playlistFile.c_str());
+  XSecLooper loop(fileNames[0]);
+  for(auto whichFile = fileNames.begin()+1; whichFile != fileNames.end(); ++whichFile)
+    {
+      loop.addFiles(*whichFile);
+    }
+
 
   // Tell the XSecLooper which neutrino type we're considering (mandatory)
-  loop.setNuPDG(14);
+  loop.setNuPDG(-14);
+  loop.setPlaylist(PlotUtils::FluxReweighter::minervame6A);
 
   // Setting the number of Universes in the GENIE error band (default 100, put 0 if you do not want to include the universes)
   loop.setNumUniv(0); 
   loop.setFiducial(5980, 8422);
 
   // Add the differential cross section dsigma/ds_dpT
-  double pt_edges[] = { 0.0, 0.075, 0.15, 0.25, 0.325, 0.4, 0.475, 0.55, 0.7, 0.85, 1.0, 1.25, 1.5, 2.5, 4.5 };
-  int pt_nbins = 14; 
+  //double pt_edges[] = { 0.0, 0.075, 0.15, 0.25, 0.325, 0.4, 0.475, 0.55, 0.7, 0.85, 1.0, 1.25, 1.5, 2.5, 4.5 };
+  //int pt_nbins = 14; 
+
+  //Hard-coded to latest optimization for carbon migration matrix...
+  //  double pt_edges[] = { 0.0, 0.09, 0.18, 0.25, 0.34, 0.425, 0.515, 0.64, 0.78, 0.94, 1.15, 1.5};
+  //int pt_nbins = 11;
+
+  //Coded for testing against the latest before the Carbon bin optimization effort
+  double pt_edges[] = { 0.0, 0.075, 0.15, 0.25, 0.325, 0.4, 0.475, 0.55, 0.7, 0.85, 1.0, 1.25, 1.5, 2.5};
+  int pt_nbins = 13;
  
   // Flux-integrated over the range 0.0 to 100.0 GeV
   MinModDepCCQEXSec* ds_dpT = new MinModDepCCQEXSec("pT");
@@ -90,13 +146,15 @@ int main(const int argc, const char** argv)
   loop.runLoop();
 
   // Get the output histograms and save them to file
-  string geniefilename =  "GENIEXSECEXTRACT_" + playlistFile.substr(playlistFile.rfind("/")+1, playlistFile.find(".")) + ".root";
+  string geniefilename =  "GENIEXSECEXTRACT_" + outName + ".root";
   TFile fout(geniefilename.c_str(), "RECREATE");
   for(uint i=0; i<loop.getXSecs().size(); ++i)
   {
     loop.getXSecs()[i]->getXSecHist()->Write();
     loop.getXSecs()[i]->getEvRateHist()->Write();
   }
+
+
 
   return 0;
 }
