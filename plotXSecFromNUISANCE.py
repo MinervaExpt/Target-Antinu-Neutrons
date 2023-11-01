f289889b2cff6da2b537b777e6be782b7a1b4615
David Last <lastd44@gmail.com>
2023-11-01 15:44:10 -0500

    Unverified changes to make the cross section calculated the signal from this analysis.

--- plotXSecFromNUISANCE.py
+++ plotXSecFromNUISANCE.py
@@ -2,7 +2,7 @@ import ROOT
 import array
 import sys
 
-def isMultiNeutron(mytree):
+def isFSSignal(mytree):
 
     #need 1 muon and no mesons and photons > 10 MeV in final state
 
@@ -11,11 +11,28 @@ def isMultiNeutron(mytree):
     pdg  = mytree.pdg
 
     nNeutrons = 0
+    nProtons = 0
+    nMuons = 0
+    nPhotons = 0
+    nMesons = 0
+    nHeavy = 0
 
     for p in range(0,nfsp):
-        if(pdg[p]==2112 and Efsp[p] > 0.010): nNeutrons += 1
+        if(pdg[p]==2112 and Efsp[p] > 0.010): 
+            nNeutrons += 1
+        elif(pdg[p]==2212 and Efsp[p] > 0.120):
+            nProtons += 1
+        elif(abs(pdg[p])==13):
+            nMuons += 1
+        elif(abs(pdg[p])==211 or abs(pdg[p])==321 or abs(pdg[p])==323 or pdg[p]==111 or pdg[p]==130 or pdg[p]==310 or pdg[p]==311 or pdg[p]==313):
+            nMesons += 1
+        elif(pdg[p]==3112 or pdg[p]==3122 or pdg[p]==3212 or pdg[p]==3222 or pdg[p]==4112 or pdg[p]==4122 or pdg[p]==4222 or pdg[p]==411 or pdg[p]==421):
+            nHeavy += 1
+        elif(pdg[p]==22 and Efsp[p] > 0.010):
+            nPhotons += 1
 
-    return nNeutrons > 1
+
+    return (nNeutrons > 0 and nMuons==1 and nMesons==0 and nProtons==0 and nPhotons==0 and nHeavy==0)
 
 def getEAvail(mytree):
   nfsp = mytree.nfsp
@@ -66,23 +83,23 @@ def isGoodMuon(mytree):
 
     muonmom = getMuonMomentum(mytree)
 
-    goodMuonMom   = muonmom.Mag()>2 and muonmom.Mag()<20
-    goodMuonAngle = muonmom.Theta()*180/3.1415 < 20
+    goodMuonMom   = muonmom.Mag()>1.5 and muonmom.Mag()<20
+    goodMuonAngle = muonmom.Theta()*180/3.1415 <= 17
 
     if goodMuonMom and goodMuonAngle: return True
     
     return False
 
 mytree = ROOT.TChain("FlatTree_VARS")
-for filename in sys.argv[1:]:
+for filename in sys.argv[2:]:
   mytree.Add(filename)
 
-ptbins = [0, 0.05, 0.11, 0.17, 0.24, 0.31, 0.4, 0.5, 0.62, 0.77, 0.95, 1.18, 1.5]
+ptbins = [0, 0.125, 0.25, 0.38, 0.515, 0.64, 0.78, 0.94, 1.15, 1.5]
 
 mypt = ROOT.TH1D("pt","pt",len(ptbins)-1,array.array("d",ptbins))
 
 for e in mytree:
-    if not (isInclusiveRHC(e) and isGoodMuon(e) and getEAvail(e) < 0.1 and isMultiNeutron(e)): continue
+    if not (isInclusiveRHC(e) and isGoodMuon(e) and isFSSignal(e)): continue
     coslep = e.CosLep
     elep= e.ELep
     fScaleFactor = e.fScaleFactor
@@ -97,8 +114,8 @@ mypt.Scale(1./mytree.GetNtrees())
 mypt.GetXaxis().SetTitle("Muon p_{t} (GeV)")
 mypt.GetYaxis().SetTitle("d#sigma/dp_{t} cm^2/GeV/nucleon")
 
-outFileName = sys.argv[1]
-outFileName = outFileName[outFileName.find("tune") + 5:outFileName.rfind("tune")] + ".root"
+outFileName = sys.argv[1]+".root"
+#outFileName = outFileName[outFileName.find("tune") + 5:outFileName.rfind("tune")] + ".root"
 myoutput = ROOT.TFile(outFileName,"CREATE")
 mypt.Write()
 
