b0bd095db580c273c11158e13c9696c7c1e36356
David Last <lastd44@gmail.com>
2024-07-08 14:21:01 -0500

    Needed change to use spack on AL9 to set up ROOT and the compiler.

--- bootstrap/setupROOT6OnGPVMs.sh
+++ bootstrap/setupROOT6OnGPVMs.sh
@@ -3,6 +3,10 @@ if [ -n "$ROOTSYS" ]; then
   echo "ROOT is already set up.  You must set up the MINERvA 101 tutorial from scratch.  Check your .bash_profile for \"root\""
 fi
 
-source /cvmfs/minerva.opensciencegrid.org/minerva/hep_hpc_products/setups
-setup root v6_10_04d -q e14:prof
-setup cmake v3_7_1
+source /cvmfs/larsoft.opensciencegrid.org/spack-packages/setup-env.sh
+spack load root@6.28.12
+spack load cmake
+spack load gcc
+#spack load fife-utils Not sure if needed
+
+export LD_LIBRARY_PATH=${ROOTSYS}/lib/root:${LD_LIBRARY_PATH}
