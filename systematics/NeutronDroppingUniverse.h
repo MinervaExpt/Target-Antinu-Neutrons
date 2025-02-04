467f537f16a1aa628ceaf394fefee3b6d4a29bb4
David Last <lastd44@gmail.com>
2025-02-04 16:16:21 -0600

    Setting up GEANT universe only which skips a candidate if the particle which created the deposit is a neutron or its parent was a neutron.

--- systematics/NeutronDroppingUniverse.h
+++ systematics/NeutronDroppingUniverse.h
@@ -1,18 +1,21 @@
 //This is meant to hold what it needs to run a universe which sometimes drops neutrons as per the prescription from Miranda Elkin's LE work.
 
+#ifndef NEUTDROPUNIV_H
+#define NEUTDROPUNIV_H
+
 #include "event/CVUniverse.h"
 
-#include "TRandom.h"
+#include "TRandom3.h"
 
 typedef std::map<std::string, std::vector<CVUniverse*>> UniverseMap;
 
-/*
 class DropGEANTNeutrons: public CVUniverse{
 public:
-DropGEANTNeutrons(/*Whatever is needed to configure the correct handling,*//*PlotUtils::ChainWrapper* chw, double prob=0.5, double thresh=10.0): CVUniverse(chw), fProb(prob), fThresh(thresh), fLeadNeutIndex(-1354), fRnd(new TRandom())
+  DropGEANTNeutrons(PlotUtils::ChainWrapper* chw, double prob=0.5, double thresh=10.0): CVUniverse(chw), m_Prob(prob), m_Thresh(thresh)
   {
-    if (fProb > 1.0) fProb = 1.0;
-    else if (fProb < 0.0) fProb = 0.0;
+    m_Rnd = new TRandom3(0);
+    if (m_Prob > 1.0) m_Prob = 1.0;
+    else if (m_Prob < 0.0) m_Prob = 0.0;
   }
   
   virtual ~DropGEANTNeutrons() = default;
@@ -26,22 +29,28 @@ DropGEANTNeutrons(/*Whatever is needed to configure the correct handling,*//*Plo
   {
     return "Drop GEANT Neutrons";
   }
-  
+
+  //This function isn't called on the truth tree so I'm not worried about needing to do anything special to make this make sense in the truth systematics.
   NeutronCandidates::NeutCands GetLeadNeutCandOnly() override
-  {
-    TString toolName = GetAnaToolName();
-    
+  {    
     std::vector<NeutronCandidates::NeutCand> cands = {};    
     int nBlobs = GetNNeutBlobs();
     
     if (nBlobs > 0){
       std::vector<double> Es = GetNeutCandEs();
-      std::vector<double> parentIDs = GetVec<double>(toolName+"_BlobParentMCPID");
+      std::string toolName = GetAnaToolName();
+      std::string branchNameParent = "_BlobParentMCPID";
+      std::string branchNamePID = "_BlobMCPID";
       int leadNeutEIndex = -999;
       double maxE = -999;
       for (unsigned int idx = 0; idx < Es.size(); ++idx){
 	if (Es.at(idx) > maxE){
-	  if ((Es.at(idx) < fThresh) && !fRnd->Binomial(1,fProb)) continue;
+	  if (Es.at(idx) < m_Thresh){
+	      int parentID = GetVecElemInt((toolName+branchNameParent).c_str(), idx);
+	      int ID = GetVecElemInt((toolName+branchNamePID).c_str(), idx);
+	      //std::cout << "parent: " << parentID << ", self: " << ID << std::endl;
+	      if ((parentID==2112 || ID==2112) && !m_Rnd->Binomial(1,m_Prob)) continue;
+	  }
 	  maxE = Es.at(idx);
 	  leadNeutEIndex = idx;
 	}
@@ -51,23 +60,23 @@ DropGEANTNeutrons(/*Whatever is needed to configure the correct handling,*//*Plo
 	cands.push_back(GetNeutCand(leadNeutEIndex));
       }
 
-      fLeadNeutIndex = leadNeutEIndex;
+      m_LeadNeutIndex = leadNeutEIndex;
     }
 
     NeutronCandidates::NeutCands EvtCands(cands);
     return EvtCands;
   }
 
-private:
-  double fProb;
-  double fThresh;
-  TRandom* fRnd;
+  private:
+  double m_Prob;
+  double m_Thresh;
+  TRandom3* m_Rnd;
 };
-*/
 
+/*
 class DropGENIENeutrons: public CVUniverse{
 public:
-  DropGENIENeutrons(/*Whatever is needed to configure the correct handling,*/PlotUtils::ChainWrapper* chw): CVUniverse(chw)
+  DropGENIENeutrons(PlotUtils::ChainWrapper* chw): CVUniverse(chw)
   {
   }
   
@@ -94,8 +103,11 @@ public:
       std::vector<double> Es = GetNeutCandEs();
       int leadNeutEIndex = std::max_element(Es.begin(),Es.end()) - Es.begin();
 
+      //This lives as a test where it shouldn't affect the total number of events.
+      cands.push_back(GetNeutCand(leadNeutEIndex));
+      
       if (Es.at(leadNeutEIndex) > 100.0){
-	cands.push_back(GetNeutCand(leadNeutEIndex));
+	m_LeadNeutIndex = leadNeutEIndex;
       }
     }
 
@@ -104,12 +116,15 @@ public:
   }
   
 };
+*/
 
-UniverseMap GetNeutronDroppingUnivs(PlotUtils::ChainWrapper* chw/*,prob*/)
+UniverseMap GetNeutronDroppingUnivs(PlotUtils::ChainWrapper* chw, double prob=0.5)
 {
   UniverseMap error_bands;
-  error_bands["DropNeutrons"].push_back(new DropGENIENeutrons(chw/*,prob*/));
-  //error_bands["DropGEANTNeutrons"].push_back(new DropGEANTNeutrons(chw, prob));
+  //error_bands["DropGENIENeutrons"].push_back(new DropGENIENeutrons(chw, prob));
+  error_bands["DropGEANTNeutrons"].push_back(new DropGEANTNeutrons(chw, prob));
 
   return error_bands;
 };
+
+#endif
