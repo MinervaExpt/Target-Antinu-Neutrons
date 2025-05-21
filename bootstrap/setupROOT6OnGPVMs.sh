354dc76ac331969082bcab897a52aab9f5b1355c
David Last <dlast@minervagpvm04.fnal.gov>
2025-05-21 09:48:40 -0500

    Change ROOT setup for SciToken Authentication.

--- bootstrap/setupROOT6OnGPVMs.sh
+++ bootstrap/setupROOT6OnGPVMs.sh
@@ -3,9 +3,9 @@ if [ -n "$ROOTSYS" ]; then
   echo "ROOT is already set up.  You must set up the MINERvA 101 tutorial from scratch.  Check your .bash_profile for \"root\""
 fi
 
-source /cvmfs/larsoft.opensciencegrid.org/spack-packages/setup-env.sh
-spack load root@6.28.12
-spack load cmake
+source /cvmfs/larsoft.opensciencegrid.org/spack-v0.22.0-fermi/setup-env.sh
+spack load root@6.28.12 arch=linux-almalinux9-x86_64_v3
+spack load cmake arch=linux-almalinux9-x86_64_v3
 spack load gcc
 #spack load fife-utils Not sure if needed
 
