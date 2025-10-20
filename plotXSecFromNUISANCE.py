d00a086eaf36c393abccee1ea533bd3604f2c035
David Last <lastd44@gmail.com>
2025-10-20 11:22:15 -0500

    Change to allow for creation of daisy-weighted distributions for the tracker cross sections for ratio comparisons.

--- plotXSecFromNUISANCE.py
+++ plotXSecFromNUISANCE.py
@@ -2,6 +2,20 @@ import ROOT
 import array
 import sys
 
+def GetIronFluxWeight(enu):
+    if (enu < 0 or enu > 12.5):
+        return 1.0
+    elif (enu > 11):
+        return 0.015323614*(enu-11)+0.97701458
+    else:
+        return 0.979706+0.0470849*enu-0.0536337*enu**2+0.0250337*enu**3-0.00515269*enu**4+0.000465905*enu**5-1.52096e-05*enu**6
+
+def GetCarbonFluxWeight(enu):
+    if (enu < 2 or enu > 10.5):
+        return 1.0
+    else:
+        return 0.338765+0.908565*enu-0.503962*enu**2+0.150799*enu**3-0.0272824*enu**4+0.00303239*enu**5-0.000188765*enu**6+4.95191e-06*enu**7
+    
 def isFSSignal(mytree):
 
     #need 1 muon and no mesons and photons > 10 MeV in final state
@@ -91,12 +105,15 @@ def isGoodMuon(mytree):
     return False
 
 mytree = ROOT.TChain("FlatTree_VARS")
-for filename in sys.argv[2:]:
+for filename in sys.argv[3:]:
   mytree.Add(filename)
+
+matFlux = int(sys.argv[2])# -1 for no modification.
 
 ptbins = [0, 0.125, 0.25, 0.38, 0.515, 0.64, 0.78, 0.94, 1.15, 1.5]
 
 mypt = ROOT.TH1D("pt","pt",len(ptbins)-1,array.array("d",ptbins))
+myFlux = ROOT.TH1D("Enu","Enu",100,0,20)
 
 for e in mytree:
     #print "E"
@@ -104,13 +121,20 @@ for e in mytree:
     #print "Got an event! YAYYYYYY!"
     coslep = e.CosLep
     elep= e.ELep
+    weight=1.0
+    if matFlux == 6:
+        weight = GetCarbonFluxWeight(e.Enu_true)
+    elif matFlux == 26:
+        weight = GetIronFluxWeight(e.Enu_true)
+
     fScaleFactor = e.fScaleFactor
   
     P = ROOT.TMath.Sqrt(elep*elep-0.105*0.105)
     Pl = coslep*P
     Pt = ROOT.TMath.Sqrt(1-coslep*coslep)*P
 
-    mypt.Fill(Pt,fScaleFactor)
+    mypt.Fill(Pt,fScaleFactor*weight)
+    myFlux.Fill(e.Enu_true,weight)
 
 mypt.Scale(1./mytree.GetNtrees())
 mypt.GetXaxis().SetTitle("Muon p_{t} (GeV)")
@@ -118,7 +142,8 @@ mypt.GetYaxis().SetTitle("d#sigma/dp_{t} cm^2/GeV/nucleon")
 
 outFileName = sys.argv[1]+".root"
 #outFileName = outFileName[outFileName.find("tune") + 5:outFileName.rfind("tune")] + ".root"
-myoutput = ROOT.TFile(outFileName,"CREATE")
+myoutput = ROOT.TFile(outFileName,"RECREATE")
 mypt.Write()
+myFlux.Write()
 
 #Remember to bin-width normalize before plotting!
